3c5b0b5ee7d8f5b8bae0a84d64146cdfa035c9c9
huudatt02 <huudatnguyen1912@gmail.com>
2025-12-13 16:25:06 +0700

    update lsp config

--- nvim/.config/nvim/lua/huudat/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/huudat/plugins/lsp/lspconfig.lua
@@ -7,41 +7,40 @@ return {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
 				local map = vim.keymap.set
-				local fzf = require("fzf-lua")
 				local opts = { buffer = ev.buf, silent = true }
 
 				opts.desc = "Show LSP references"
-				map("n", "gr", fzf.lsp_references, opts)
+				map("n", "gr", "<cmd>FzfLua lsp_references<CR>", opts)
 
 				opts.desc = "Show LSP definitions"
-				map("n", "gd", fzf.lsp_definitions, opts)
+				map("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", opts)
 
 				opts.desc = "Go to declaration"
-				map("n", "gD", fzf.lsp_declarations, opts)
+				map("n", "gD", "<cmd>FzfLua lsp_declarations<CR>", opts)
 
 				opts.desc = "Show LSP implementations"
-				map("n", "gi", fzf.lsp_implementations, opts)
+				map("n", "gi", "<cmd>FzfLua lsp_implementations<CR>", opts)
 
 				opts.desc = "Show LSP type definitions"
-				map("n", "gt", fzf.lsp_typedefs, opts)
+				map("n", "gt", "<cmd>FzfLua lsp_typedefs<CR>", opts)
 
 				opts.desc = "Show document symbols"
-				map("n", "<leader>ls", fzf.lsp_document_symbols, opts)
+				map("n", "<leader>ls", "<cmd>FzfLua lsp_document_symbols<CR>", opts)
 
 				opts.desc = "Show workspace symbols"
-				map("n", "<leader>lS", fzf.lsp_workspace_symbols, opts)
+				map("n", "<leader>lS", "<cmd>FzfLua lsp_workspace_symbols<CR>", opts)
 
 				opts.desc = "See available code actions"
-				map({ "n", "v" }, "<leader>ca", fzf.lsp_code_actions, opts)
+				map({ "n", "v" }, "<leader>ca", "<cmd>FzfLua lsp_code_actions<CR>", opts)
 
 				opts.desc = "Smart rename"
 				map("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
 				opts.desc = "Show document diagnostics"
-				map("n", "<leader>ld", fzf.diagnostics_document, opts)
+				map("n", "<leader>ld", "<cmd>FzfLua diagnostics_document<CR>", opts)
 
 				opts.desc = "Show workspace diagnostics"
-				map("n", "<leader>lD", fzf.diagnostics_workspace, opts)
+				map("n", "<leader>lD", "<cmd>FzfLua diagnostics_workspace<CR>", opts)
 
 				opts.desc = "Show documentation for what is under cursor"
 				map("n", "K", vim.lsp.buf.hover, opts)

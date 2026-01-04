b50392b47b3b815b7821fb27aa3ec1f74a673eda
huudatt02 <huudatnguyen1912@gmail.com>
2026-01-05 00:49:01 +0700

    feat(fzf): add opinionated fzf-lua keymaps

--- nvim/.config/nvim/lua/huudat/plugins/fzf.lua
+++ nvim/.config/nvim/lua/huudat/plugins/fzf.lua
@@ -1,26 +1,37 @@
 return {
 	"ibhagwan/fzf-lua",
+	cmd = "FzfLua",
 	dependencies = { "nvim-mini/mini.icons" },
 	opts = {},
 	keys = {
 		-- Files
 		{ "<leader>ff", "<cmd>FzfLua files<CR>", desc = "Files" },
 		{ "<leader>fb", "<cmd>FzfLua buffers<CR>", desc = "Buffers" },
+		{ "<leader>fl", "<cmd>FzfLua lines<CR>", desc = "Buffers Lines" },
+		{ "<leader>fL", "<cmd>FzfLua blines<CR>", desc = "Current Buffer Lines" },
 		{ "<leader>fo", "<cmd>FzfLua oldfiles<CR>", desc = "Oldfiles" },
-		{ "<leader>fh", "<cmd>FzfLua help_tags<CR>", desc = "Help Tags" },
 
 		-- Search
 		{ "<leader>sg", "<cmd>FzfLua grep<CR>", desc = "Grep" },
-		{ "<leader>sl", "<cmd>FzfLua live_grep<CR>", desc = "Live Grep" },
+		{ "<leader>sG", "<cmd>FzfLua live_grep<CR>", desc = "Live Grep" },
 		{ "<leader>sw", "<cmd>FzfLua grep_cword<CR>", desc = "Grep Word" },
 		{ "<leader>sW", "<cmd>FzfLua grep_cWORD<CR>", desc = "Grep WORD" },
+
+		-- Quickfix / Loclist
+		{ "<leader>sq", "<cmd>FzfLua quickfix<CR>", desc = "Quickfix List" },
+		{ "<leader>sQ", "<cmd>FzfLua lgrep_quickfix<CR>", desc = "Search Quickfix" },
+		{ "<leader>sl", "<cmd>FzfLua loclist<CR>", desc = "Location List" },
+		{ "<leader>sL", "<cmd>FzfLua lgrep_loclist<CR>", desc = "Search Loclist" },
 
 		-- Git
 		{ "<leader>gf", "<cmd>FzfLua git_files<CR>", desc = "Git Files" },
 		{ "<leader>gs", "<cmd>FzfLua git_status<CR>", desc = "Git Status" },
+		{ "<leader>gS", "<cmd>FzfLua git_stash<CR>", desc = "Git Stash" },
 		{ "<leader>gb", "<cmd>FzfLua git_branches<CR>", desc = "Git Branches" },
+		{ "<leader>gB", "<cmd>FzfLua git_blame<CR>", desc = "Git Blame" },
 		{ "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Git Commits" },
 		{ "<leader>gC", "<cmd>FzfLua git_bcommits<CR>", desc = "Git Bcommits" },
+		{ "<leader>gd", "<cmd>FzfLua git_diff HEAD<CR>", desc = "Git Diff (HEAD)" },
 
 		-- Misc
 		{ "<leader>ut", "<cmd>FzfLua undotree<CR>", desc = "Undotree" },

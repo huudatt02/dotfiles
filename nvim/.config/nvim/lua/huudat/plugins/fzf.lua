738a94408a22b8ea992aaeb6fa86e20cf1f012a7
huudatt02 <huudatnguyen1912@gmail.com>
2026-02-20 22:21:47 +0700

    chore(nvim): update plugins

--- nvim/.config/nvim/lua/huudat/plugins/fzf.lua
+++ nvim/.config/nvim/lua/huudat/plugins/fzf.lua
@@ -43,18 +43,17 @@ return {
 		-- Files
 		{ "<leader>ff", "<cmd>FzfLua files<CR>", desc = "Files" },
 		{ "<leader>fb", "<cmd>FzfLua buffers<CR>", desc = "Buffers" },
-		{ "<leader>fg", "<cmd>FzfLua git_files<CR>", desc = "Git Files" },
 		{ "<leader>fl", "<cmd>FzfLua blines<CR>", desc = "Current Buffer Lines" },
 
 		-- Search
-		{ "<leader>sg", "<cmd>FzfLua grep<CR>", desc = "Grep" },
-		{ "<leader>sG", "<cmd>FzfLua live_grep<CR>", desc = "Live Grep" },
+		{ "<leader>sg", "<cmd>FzfLua live_grep<CR>", desc = "Live Grep" },
 		{ "<leader>sw", "<cmd>FzfLua grep_cword<CR>", desc = "Grep Word" },
 		{ "<leader>sW", "<cmd>FzfLua grep_cWORD<CR>", desc = "Grep WORD" },
 		{ "<leader>sq", "<cmd>FzfLua quickfix<CR>", desc = "Quickfix List" },
 		{ "<leader>sl", "<cmd>FzfLua loclist<CR>", desc = "Location List" },
 
 		-- Git
+		{ "<leader>gf", "<cmd>FzfLua git_files<CR>", desc = "Git Files" },
 		{ "<leader>gs", "<cmd>FzfLua git_status<CR>", desc = "Git Status" },
 		{ "<leader>gb", "<cmd>FzfLua git_branches<CR>", desc = "Git Branches" },
 		{ "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Git Commits" },

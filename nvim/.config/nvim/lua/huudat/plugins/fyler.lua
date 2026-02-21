75c5d0a6fc7cf8cd8cab3e3b1427eb6a31388bde
huudatt02 <huudatnguyen1912@gmail.com>
2026-02-21 10:32:41 +0700

    chore(nvim): update plugins

--- nvim/.config/nvim/lua/huudat/plugins/fyler.lua
+++ nvim/.config/nvim/lua/huudat/plugins/fyler.lua
@@ -24,7 +24,7 @@ return {
 							Modified = "󰜥 ",
 							Deleted = " ",
 							Renamed = "󰁕 ",
-							Copied = " ",
+							Copied = " ",
 							Conflict = " ",
 							Ignored = " ",
 						},

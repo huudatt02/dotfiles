b5c0db9b57f3523d0fbc8e85d8d9e54691dde3f7
huudatt02 <huudatnguyen1912@gmail.com>
2026-02-28 19:31:51 +0700

    chore(nvim): update plugins

--- nvim/.config/nvim/lua/huudat/plugins/neotree.lua
+++ nvim/.config/nvim/lua/huudat/plugins/neotree.lua
@@ -37,6 +37,16 @@ return {
 			mappings = {
 				["l"] = "toggle_node",
 				["h"] = "close_node",
+				["Y"] = {
+					function(state)
+						local node = state.tree:get_node()
+						local path = node:get_id()
+						local rel_path = vim.fn.fnamemodify(path, ":.")
+
+						vim.fn.setreg("+", rel_path)
+						vim.notify("Copied relative path: " .. rel_path)
+					end,
+				},
 			},
 		},
 		filesystem = {

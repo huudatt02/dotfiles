e00e51f2b8207708e936828b7b3b318079d4b3aa
huudatt02 <huudatnguyen1912@gmail.com>
2026-03-14 23:27:27 +0700

    chore(opencode): refactor keymap

--- nvim/.config/nvim/lua/huudat/plugins/opencode.lua
+++ nvim/.config/nvim/lua/huudat/plugins/opencode.lua
@@ -30,16 +30,15 @@ return {
 
 		vim.o.autoread = true
 
-		-- Recommended/example keymaps.
-		vim.keymap.set({ "n", "x" }, "<C-a>", function()
-			require("opencode").ask("@this: ", { submit = true })
-		end, { desc = "Ask opencode" })
-		vim.keymap.set({ "n", "x" }, "<C-x>", function()
-			require("opencode").select()
-		end, { desc = "Execute opencode action…" })
-		vim.keymap.set({ "n", "t" }, "<C-.>", function()
+		vim.keymap.set({ "n", "t" }, "<leader>ot", function()
 			require("opencode").toggle()
 		end, { desc = "Toggle opencode" })
+		vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+			require("opencode").ask("@this: ", { submit = true })
+		end, { desc = "Ask opencode" })
+		vim.keymap.set({ "n", "x" }, "<leader>oe", function()
+			require("opencode").select()
+		end, { desc = "Execute opencode action…" })
 
 		vim.keymap.set({ "n", "x" }, "go", function()
 			return require("opencode").operator("@this ")
@@ -54,9 +53,5 @@ return {
 		vim.keymap.set("n", "<S-C-d>", function()
 			require("opencode").command("session.half.page.down")
 		end, { desc = "opencode half page down" })
-
-		-- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o".
-		vim.keymap.set("n", "+", "<C-a>", { desc = "Increment", noremap = true })
-		vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement", noremap = true })
 	end,
 }

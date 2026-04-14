928a1f7ededfd7a766b44b7a1a623b5134d3be40
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-14 22:45:25 +0700

    chore(nvim): add plugin better-escape

--- nvim/.config/nvim/lua/huudat/plugins/tools.lua
+++ nvim/.config/nvim/lua/huudat/plugins/tools.lua
@@ -92,6 +92,18 @@ return {
   },
 
   {
+    "max397574/better-escape.nvim",
+    event = "VeryLazy",
+    opts = {
+      timeout = 300,
+      default_mappings = false,
+      mappings = {
+        i = { j = { k = "<Esc>", j = "<Esc>" } },
+      },
+    },
+  },
+
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = "markdown",
     dependencies = {

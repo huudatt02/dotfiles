5e6d8208bfa390f68c73bc6949009c814382f0f8
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-15 19:44:21 +0700

    chore(nvim): config xcodebuild plugin

--- nvim/.config/nvim/lua/huudat/plugins/xcodebuild.lua
+++ nvim/.config/nvim/lua/huudat/plugins/xcodebuild.lua
@@ -1,9 +1,22 @@
 return {
   "wojciech-kulik/xcodebuild.nvim",
+  ft = { "swift" },
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-neo-tree/neo-tree.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  opts = {},
+  opts = {
+    show_build_progress_bar = true,
+    logs = {
+      auto_open_on_success_tests = false,
+      auto_open_on_failed_tests = false,
+      auto_open_on_success_build = false,
+      auto_open_on_failed_build = true,
+      auto_focus = false,
+    },
+    code_coverage = {
+      enabled = true,
+    },
+  },
 }

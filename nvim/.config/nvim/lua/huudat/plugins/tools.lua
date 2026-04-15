5fe202f980fe5d881035d2ce57728985df8a2af0
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-15 09:45:22 +0700

    chore(nvim): remove nvim-ufo

--- nvim/.config/nvim/lua/huudat/plugins/tools.lua
+++ nvim/.config/nvim/lua/huudat/plugins/tools.lua
@@ -69,29 +69,6 @@ return {
   },
 
   {
-    "kevinhwang91/nvim-ufo",
-    event = "BufReadPost",
-    dependencies = {
-      "kevinhwang91/promise-async",
-    },
-    config = function()
-      vim.opt.foldcolumn = "0"
-      vim.opt.foldlevel = 99
-      vim.opt.foldlevelstart = 99
-      vim.opt.foldenable = true
-
-      require("ufo").setup({
-        provider_selector = function(bufnr, filetype, buftype)
-          if filetype == "neo-tree" then
-            return ""
-          end
-          return { "treesitter", "indent" }
-        end,
-      })
-    end,
-  },
-
-  {
     "max397574/better-escape.nvim",
     event = "VeryLazy",
     opts = {

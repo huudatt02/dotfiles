7f6e1d61cfb94f483948899e1ffaba31c9416f72
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-16 00:04:52 +0700

    chore(nvim): update keymaps xcodebuild plugin

--- nvim/.config/nvim/lua/huudat/plugins/xcodebuild.lua
+++ nvim/.config/nvim/lua/huudat/plugins/xcodebuild.lua
@@ -20,29 +20,27 @@ return {
     },
   },
   keys = {
-    -- Build & Run
+    -- build & run
     { "<leader>xb", "<cmd>XcodebuildBuild<cr>", desc = "Build" },
     { "<leader>xB", "<cmd>XcodebuildBuildForTesting<cr>", desc = "Build for Testing" },
     { "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", desc = "Build & Run" },
     { "<leader>xR", "<cmd>XcodebuildRun<cr>", desc = "Run (no build)" },
 
-    -- Tests
+    -- tests
     { "<leader>xt", "<cmd>XcodebuildTest<cr>", desc = "Run Tests" },
     { "<leader>xT", "<cmd>XcodebuildTestClass<cr>", desc = "Test Current Class" },
 
-    -- Project
+    -- project
     { "<leader>xs", "<cmd>XcodebuildSetup<cr>", desc = "Setup Project" },
     { "<leader>xS", "<cmd>XcodebuildSelectScheme<cr>", desc = "Select Scheme" },
     { "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", desc = "Select Device" },
-    { "<leader>xP", "<cmd>XcodebuildPicker<cr>", desc = "Xcode Picker" },
+    { "<leader>xp", "<cmd>XcodebuildPicker<cr>", desc = "Xcode Picker" },
 
-    -- Utilities
+    -- utilities
     { "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", desc = "Toggle Logs" },
     { "<leader>xc", "<cmd>XcodebuildCleanBuild<cr>", desc = "Clean Build" },
-    { "<leader>xq", "<cmd>XcodebuildQuickfixLine<cr>", desc = "Quickfix Line" },
-    { "<leader>xa", "<cmd>XcodebuildCodeActions<cr>", desc = "Code Actions" },
 
-    -- Simulator
+    -- simulator
     { "<leader>xo", "<cmd>XcodebuildBootSimulator<cr>", desc = "Boot Simulator" },
     { "<leader>xi", "<cmd>XcodebuildInstallApp<cr>", desc = "Install App" },
     { "<leader>xu", "<cmd>XcodebuildUninstallApp<cr>", desc = "Uninstall App" },

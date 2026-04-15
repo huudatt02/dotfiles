07e7c40910cae5905506c7a04c60765de3891804
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-15 18:13:03 +0700

    chore(nvim): add xcodebuild plugin

---
+++ nvim/.config/nvim/lua/huudat/plugins/xcodebuild.lua
@@ -0,0 +1,9 @@
+return {
+  "wojciech-kulik/xcodebuild.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {},
+}

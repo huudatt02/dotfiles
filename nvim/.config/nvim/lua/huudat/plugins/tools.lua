670b3b8c70461a3a5078e47b8457eb7a9aaef0f6
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-17 17:09:03 +0700

    chore(nvim): add which-key plugin

--- nvim/.config/nvim/lua/huudat/plugins/tools.lua
+++ nvim/.config/nvim/lua/huudat/plugins/tools.lua
@@ -69,6 +69,38 @@ return {
   },
 
   {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      preset = "helix",
+      spec = {
+        mode = { "n", "x" },
+        { "<leader>c", group = "code" },
+        { "<leader>d", group = "debug" },
+        { "<leader>e", desc = "explorer" },
+        { "<leader>f", group = "file/find" },
+        { "<leader>g", group = "git" },
+        { "<leader>gh", group = "hunks" },
+        { "<leader>l", group = "lsp" },
+        { "<leader>q", group = "quit/session" },
+        { "<leader>s", group = "search" },
+        { "<leader>u", group = "ui" },
+        { "<leader>x", group = "xcode" },
+        { "[", group = "prev" },
+        { "]", group = "next" },
+        { "g", group = "goto" },
+        { "gs", group = "surround" },
+        { "z", group = "fold" },
+      },
+    },
+    -- stylua: ignore start
+    keys = {
+      { "<leader>?", function() require("which-key").show({ global = false }) end, desc = "Buffer Local Keymaps (which-key)" },
+    },
+    -- stylua: ignore end
+  },
+
+  {
     "max397574/better-escape.nvim",
     event = "VeryLazy",
     opts = {

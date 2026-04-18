52de641211831ea7d3675f46a32af48b68283a42
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-18 17:14:51 +0700

    chore(nvim): update conform.nvim config

--- nvim/.config/nvim/lua/huudat/plugins/formatting.lua
+++ nvim/.config/nvim/lua/huudat/plugins/formatting.lua
@@ -30,6 +30,10 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("conform").setup(opts)
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
   keys = {
     {
       "<leader>cf",

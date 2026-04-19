875f36cc463155fa3102bb9637854df15683c296
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-19 23:40:42 +0700

    chore(nvim): update swift snippet config

--- nvim/.config/nvim/lua/huudat/snippets/swift.lua
+++ nvim/.config/nvim/lua/huudat/snippets/swift.lua
@@ -4,27 +4,43 @@ local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
-  s("ifcaselet", fmt([[
-    if case let {} = {} {{
-      {}
-    }}
+    s("ifcaselet", fmt([[
+if case let {} = {} {{
     {}
-  ]], { i(1, "pattern"), i(2, "value"), i(3), i(0) })),
+}}
+{}
+]], {
+        i(1, "pattern"),
+        i(2, "value"),
+        i(3),
+        i(0),
+    })),
 
-  s("funcasync", fmt([[
-    func {}({}) async{} -> {} {{
-      {}
-    }}
+    s("funcasync", fmt([[
+func {}({}) async{} -> {} {{
     {}
-  ]], { i(1, "name"), i(2), i(3), i(4, "Void"), i(5), i(0) })),
+}}
+{}
+]], {
+        i(1, "name"),
+        i(2),
+        i(3),
+        i(4, "Void"),
+        i(5),
+        i(0),
+    })),
 
-  s("main", fmt([[
-    @main
-    public struct {} {{
-      public static func main() {{
+    s("main", fmt([[
+@main
+public struct {} {{
+    public static func main() {{
         {}
-      }}
     }}
-    {}
-  ]], { i(1, "App"), i(2), i(0) })),
+}}
+{}
+]], {
+        i(1, "App"),
+        i(2),
+        i(0),
+    })),
 }

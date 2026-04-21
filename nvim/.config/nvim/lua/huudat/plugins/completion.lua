006c6aab7179812d03ec3b6614af229ea5552bed
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-21 16:57:24 +0700

    chore(nvim): update kind_icons blink.cmp config

--- nvim/.config/nvim/lua/huudat/plugins/completion.lua
+++ nvim/.config/nvim/lua/huudat/plugins/completion.lua
@@ -17,6 +17,38 @@ return {
     },
     appearance = {
       nerd_font_variant = "mono",
+      kind_icons = {
+        Text = "󰉿",
+        Method = "󰊕",
+        Function = "󰊕",
+        Constructor = "󰒓",
+
+        Field = "󰜢",
+        Variable = "󰆦",
+        Property = "󰖷",
+
+        Class = "󱡠",
+        Interface = "󱡠",
+        Struct = "󱡠",
+        Module = "󰅩",
+
+        Unit = "󰪚",
+        Value = "󰦨",
+        Enum = "󰦨",
+        EnumMember = "󰦨",
+
+        Keyword = "󰻾",
+        Constant = "󰏿",
+
+        Snippet = "󱄽",
+        Color = "󰏘",
+        File = "󰈔",
+        Reference = "󰬲",
+        Folder = "󰉋",
+        Event = "󱐋",
+        Operator = "󰪚",
+        TypeParameter = "󰬛",
+      },
     },
     completion = {
       accept = {

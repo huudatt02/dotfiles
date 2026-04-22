24e685a2ce15cb07441cebef59071bd0d779958c
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-23 00:17:22 +0700

    chore(nvim): update filename config

---
+++ nvim/.config/nvim/lua/huudat/plugins/todo-comments.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/todo-comments.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {},
+  -- stylua: ignore start
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+    { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+    { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+  },
+  -- stylua: ignore end
+}

3a5494b6817ba99136bca1ff251df10583fb9190
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-22 23:58:46 +0700

    chore(nvim): update filename config

--- nvim/.config/nvim/lua/huudat/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/huudat/plugins/treesitter.lua
@@ -1,60 +1,68 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter").install({
-      -- dart
-      "dart",
-      -- swift
-      "swift",
-      -- nodejs
-      "tsx",
-      "typescript",
-      "javascript",
-      -- golang
-      "go",
-      "gomod",
-      "gosum",
-      "gowork",
-      -- java
-      "java",
-      "javadoc",
-      "kotlin",
-      "groovy",
-      -- config / data
-      "bash",
-      "lua",
-      "sql",
-      "xml",
-      "yaml",
-      "json",
-      "toml",
-      "regex",
-      "proto",
-      "graphql",
-      "gitignore",
-      "dockerfile",
-      -- docs & editor
-      "markdown",
-      "markdown_inline",
-    })
-    vim.api.nvim_create_autocmd("FileType", {
-      callback = function(ev)
-        if not pcall(vim.treesitter.start, ev.buf) then
-          return
-        end
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").install({
+        -- dart
+        "dart",
+        -- swift
+        "swift",
+        -- nodejs
+        "tsx",
+        "typescript",
+        "javascript",
+        -- golang
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+        -- java
+        "java",
+        "javadoc",
+        "kotlin",
+        "groovy",
+        -- config / data
+        "bash",
+        "lua",
+        "sql",
+        "xml",
+        "yaml",
+        "json",
+        "toml",
+        "regex",
+        "proto",
+        "graphql",
+        "gitignore",
+        "dockerfile",
+        -- docs & editor
+        "markdown",
+        "markdown_inline",
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(ev)
+          if not pcall(vim.treesitter.start, ev.buf) then
+            return
+          end
 
-        local lang = vim.treesitter.language.get_lang(ev.match)
-        if lang and vim.treesitter.query.get(lang, "indents") then
-          vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end
+          local lang = vim.treesitter.language.get_lang(ev.match)
+          if lang and vim.treesitter.query.get(lang, "indents") then
+            vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
 
-        if lang and vim.treesitter.query.get(lang, "folds") then
-          vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-          vim.wo[0][0].foldmethod = "expr"
-        end
-      end,
-    })
-  end,
+          if lang and vim.treesitter.query.get(lang, "folds") then
+            vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.wo[0][0].foldmethod = "expr"
+          end
+        end,
+      })
+    end,
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {},
+  },
 }

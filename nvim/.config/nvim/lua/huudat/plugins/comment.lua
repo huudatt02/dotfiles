ddee10777ccc97b8f1739b7db4a8df1b22d30233
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-22 16:58:04 +0700

    chore(nvim): update filename config

--- nvim/.config/nvim/lua/huudat/plugins/comment.lua
+++ nvim/.config/nvim/lua/huudat/plugins/comment.lua
@@ -1,32 +1,15 @@
 return {
-  {
-    "numToStr/Comment.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-    config = function()
-      require("ts_context_commentstring").setup({
-        enable_autocmd = false,
-      })
-      require("Comment").setup({
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      })
-    end,
+  "numToStr/Comment.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
   },
-
-  {
-    "folke/todo-comments.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
-    -- stylua: ignore start
-    keys = {
-      { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
-      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-      { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
-      { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
-    },
-    -- stylua: ignore end
-  },
+  config = function()
+    require("ts_context_commentstring").setup({
+      enable_autocmd = false,
+    })
+    require("Comment").setup({
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    })
+  end,
 }

845a750df54d9302db254897c12f345607d9ef96
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-22 14:34:44 +0700

    chore(nvim): update colorizer config

--- nvim/.config/nvim/lua/huudat/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/huudat/plugins/colorizer.lua
@@ -1,28 +1,26 @@
 return {
-  {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = {
-      filetypes = {
-        "dart",
-        "html",
-        "css",
-        "javascriptreact",
-        "typescriptreact",
-      },
-      options = {
-        parsers = {
-          css = false,
-          css_fn = false,
-          names = { enable = false },
-          hex = {
-            rgb = true,
-            rgba = true,
-            rrggbb = true,
-            aarrggbb = true,
-          },
-          tailwind = { enable = true },
+  "catgoose/nvim-colorizer.lua",
+  event = "BufReadPre",
+  opts = {
+    filetypes = {
+      "dart",
+      "html",
+      "css",
+      "javascriptreact",
+      "typescriptreact",
+    },
+    options = {
+      parsers = {
+        css = false,
+        css_fn = false,
+        names = { enable = false },
+        hex = {
+          rgb = true,
+          rgba = true,
+          rrggbb = true,
+          aarrggbb = true,
         },
+        tailwind = { enable = true },
       },
     },
   },

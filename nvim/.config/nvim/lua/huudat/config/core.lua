d77bd4052d4b7e0cfe198c83a25f27be6e362558
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-24 18:01:56 +0700

    chore(nvim): add file core

---
+++ nvim/.config/nvim/lua/huudat/config/core.lua
@@ -0,0 +1,47 @@
+local M = {}
+
+M.icons = {
+  kinds = {
+    Text = "󰉿 ",
+    Method = "󰊕 ",
+    Function = "󰊕 ",
+    Constructor = "󰒓 ",
+
+    Field = "󰜢 ",
+    Variable = "󰀫 ",
+    Property = "󰖷 ",
+
+    Class = "󰠱 ",
+    Interface = "󰒪 ",
+    Struct = "󰙅 ",
+    Module = "󰆧 ",
+
+    Unit = "󰑭 ",
+    Value = "󰎠 ",
+    Enum = "󰈍 ",
+    EnumMember = "󰈍 ",
+
+    Keyword = "󰻾 ",
+    Constant = "󰏿 ",
+
+    Snippet = "󰅴 ",
+    Color = "󰏘 ",
+    File = "󰈔 ",
+    Reference = "󰬲 ",
+    Folder = "󰉋 ",
+    Event = "󱐋 ",
+    Operator = "󰪚 ",
+    TypeParameter = "󰬛 ",
+  },
+  diagnostics = {
+    Error = " ",
+    Warn = " ",
+    Hint = " ",
+    Info = " ",
+  },
+  git = {
+    added = " ",
+    modified = "󰜥 ",
+    removed = " ",
+  },
+}

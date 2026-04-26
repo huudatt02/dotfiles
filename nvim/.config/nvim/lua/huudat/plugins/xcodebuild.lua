0a153fdcb5a6dd20bf7695344f0f0fd6b018d86a
huudatt02 <huudatnguyen1912@gmail.com>
2026-04-27 01:33:56 +0700

    chore(nvim): update file core

--- nvim/.config/nvim/lua/huudat/plugins/xcodebuild.lua
+++ nvim/.config/nvim/lua/huudat/plugins/xcodebuild.lua
@@ -23,22 +23,27 @@ return {
 
     local map = vim.keymap.set
 
+    -- build & run
     map("n", "<leader>xb", "<cmd>XcodebuildBuild<cr>", { desc = "Build Project" })
     map("n", "<leader>xB", "<cmd>XcodebuildBuildForTesting<cr>", { desc = "Build For Testing" })
     map("n", "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", { desc = "Build & Run Project" })
     map("n", "<leader>xR", "<cmd>XcodebuildRun<cr>", { desc = "Run Project" })
 
+    -- tests
     map("n", "<leader>xt", "<cmd>XcodebuildTest<cr>", { desc = "Run Tests" })
     map("n", "<leader>xT", "<cmd>XcodebuildTestClass<cr>", { desc = "Run Current Test Class" })
 
+    -- project
     map("n", "<leader>xp", "<cmd>XcodebuildPicker<cr>", { desc = "Xcode Picker" })
     map("n", "<leader>xs", "<cmd>XcodebuildSetup<cr>", { desc = "Setup Project" })
     map("n", "<leader>xS", "<cmd>XcodebuildSelectScheme<cr>", { desc = "Select Scheme" })
     map("n", "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
 
+    -- utilities
     map("n", "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", { desc = "Toggle Logs" })
     map("n", "<leader>xc", "<cmd>XcodebuildCleanBuild<cr>", { desc = "Clean Build" })
 
+    -- simulator
     map("n", "<leader>xo", "<cmd>XcodebuildBootSimulator<cr>", { desc = "Boot Simulator" })
     map("n", "<leader>xi", "<cmd>XcodebuildInstallApp<cr>", { desc = "Install App" })
     map("n", "<leader>xu", "<cmd>XcodebuildUninstallApp<cr>", { desc = "Uninstall App" })

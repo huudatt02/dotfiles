532460cc471ec5bc1462b4bdfe6b5ab55912eac6
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-12 21:51:34 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/plugins/volume.sh
+++ sketchybar/.config/sketchybar/plugins/volume.sh
@@ -4,13 +4,10 @@ if [ "$SENDER" = "volume_change" ]; then
   VOLUME="$INFO"
 
   case "$VOLUME" in
-    [6-9][0-9]|100) ICON="󰕾"
-    ;;
-    [3-5][0-9]) ICON="󰖀"
-    ;;
-    [1-9]|[1-2][0-9]) ICON="󰕿"
-    ;;
-    *) ICON="󰖁"
+    [5-9][1-9]|100) ICON="􀊩" ;;
+    50) ICON="􀊥" ;;
+    [1-4][0-9]) ICON="􀊥" ;;
+    *) ICON="󰖁" ;;
   esac
 
   sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"

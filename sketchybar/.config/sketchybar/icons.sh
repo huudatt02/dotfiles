f9d06fa36821737819f8a122b1d12ea0d1382fa8
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-13 15:24:13 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/icons.sh
+++ sketchybar/.config/sketchybar/icons.sh
@@ -9,27 +9,6 @@ LOCK=􀒳
 BELL=􀋚
 BELL_DOT=􀝗
 
-# Git Icons
-GIT_ISSUE=􀍷
-GIT_DISCUSSION=􀒤
-GIT_PULL_REQUEST=􀙡
-GIT_COMMIT=􀡚
-GIT_INDICATOR=􀂓
-
-# Spotify Icons
-SPOTIFY_BACK=􀊎
-SPOTIFY_PLAY_PAUSE=􀊈
-SPOTIFY_NEXT=􀊐
-SPOTIFY_SHUFFLE=􀊝
-SPOTIFY_REPEAT=􀊞
-
-# Yabai Icons
-YABAI_STACK=􀏭
-YABAI_FULLSCREEN_ZOOM=􀏜
-YABAI_PARENT_ZOOM=􀥃
-YABAI_FLOAT=􀢌
-YABAI_GRID=􀧍
-
 # Battery Icons
 BATTERY_100=􀛨
 BATTERY_75=􀺸
@@ -48,10 +27,3 @@ VOLUME_0=􀊣
 # WiFi
 WIFI_CONNECTED=􀙇
 WIFI_DISCONNECTED=􀙈
-
-# svim
-MODE_NORMAL=􀂯
-MODE_INSERT=􀂥
-MODE_VISUAL=􀂿
-MODE_CMD=􀂙
-MODE_PENDING=􀈏

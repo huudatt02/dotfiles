1500873348a988713ee4d2974cb5ca5e5529e343
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-13 17:45:19 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/colors.sh
+++ sketchybar/.config/sketchybar/colors.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-### Catppuccin
 export BLACK=0xff181926
 export WHITE=0xffcad3f5
-export RED=0xffed8796
-export GREEN=0xffa6da95
+export RED=0xffff757f
+export GREEN=0xffc3e88d
 export BLUE=0xff8aadf4
-export YELLOW=0xffeed49f
+export YELLOW=0xffffc777
 export ORANGE=0xfff5a97f
 export MAGENTA=0xffc6a0f6
 export GREY=0xff939ab7
+
 export TRANSPARENT=0x00000000
 export BG0=0xff1e1e2e
 export BG1=0x603c3e4f

b321510320d359c09cd54707ae258933bf76baf2
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-14 21:43:04 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/plugins/wifi.sh
+++ sketchybar/.config/sketchybar/plugins/wifi.sh
@@ -2,7 +2,7 @@
 
 source "$CONFIG_DIR/icons.sh"
 
-IP="$(ipconfig getifaddr en0)"
+IP="$(ipconfig getifaddr en0 2>/dev/null)"
 ICON="$([ -n "$IP" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
 
 sketchybar --set "$NAME" icon="$ICON"

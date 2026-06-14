13482222e3dd0cf9ed93e5f1ed8d2abdf519cca6
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-15 00:28:25 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/plugins/icon_map.sh
+++ sketchybar/.config/sketchybar/plugins/icon_map.sh
@@ -24,11 +24,38 @@ function icon_map() {
   "Calculator")
     icon_result=":calculator:"
     ;;
+  "Code" | "Code - Insiders")
+    icon_result=":code:"
+    ;;
+  "Cursor")
+    icon_result=":cursor:"
+    ;;
   "DataGrip")
     icon_result=":datagrip:"
     ;;
+  "DataSpell")
+    icon_result=":dataspell:"
+    ;;
+  "Discord" | "Discord Canary" | "Discord PTB")
+    icon_result=":discord:"
+    ;;
+  "Docker" | "Docker Desktop")
+    icon_result=":docker:"
+    ;;
+  "Drafts")
+    icon_result=":drafts:"
+    ;;
+  "draw.io")
+    icon_result=":draw_io:"
+    ;;
   "FaceTime")
     icon_result=":face_time:"
+    ;;
+  "Figma")
+    icon_result=":figma:"
+    ;;
+  "Final Cut Pro")
+    icon_result=":final_cut_pro:"
     ;;
   "Finder")
     icon_result=":finder:"
@@ -39,8 +66,17 @@ function icon_map() {
   "Firefox")
     icon_result=":firefox:"
     ;;
+  "GitHub Desktop")
+    icon_result=":git_hub:"
+    ;;
   "GoLand")
     icon_result=":goland:"
+    ;;
+  "IINA")
+    icon_result=":iina:"
+    ;;
+  "Insomnia")
+    icon_result=":insomnia:"
     ;;
   "IntelliJ IDEA")
     icon_result=":idea:"
@@ -50,6 +86,12 @@ function icon_map() {
     ;;
   "kitty")
     icon_result=":kitty:"
+    ;;
+  "LM Studio")
+    icon_result=":lm_studio:"
+    ;;
+  "MAMP" | "MAMP PRO")
+    icon_result=":mamp:"
     ;;
   "Maps" | "Google Maps")
     icon_result=":maps:"
@@ -66,17 +108,38 @@ function icon_map() {
   "Music")
     icon_result=":music:"
     ;;
+  "Neovide" | "neovide")
+    icon_result=":neovide:"
+    ;;
+  "Neovim" | "neovim" | "nvim")
+    icon_result=":neovim:"
+    ;;
   "Notes")
     icon_result=":notes:"
     ;;
+  "Obsidian")
+    icon_result=":obsidian:"
+    ;;
+  "OBS")
+    icon_result=":obsstudio:"
+    ;;
+  "Preview")
+    icon_result=":pdf:"
+    ;;
   "Podcasts")
     icon_result=":podcasts:"
+    ;;
+  "Postman")
+    icon_result=":postman:"
     ;;
   "PhpStorm")
     icon_result=":php_storm:"
     ;;
   "PyCharm")
     icon_result=":pycharm:"
+    ;;
+  "qutebrowser")
+    icon_result=":qute_browser:"
     ;;
   "Reminders")
     icon_result=":reminders:"
@@ -105,8 +168,17 @@ function icon_map() {
   "System Preferences" | "System Settings")
     icon_result=":gear:"
     ;;
+  "Telegram")
+    icon_result=":telegram:"
+    ;;
   "Terminal")
     icon_result=":terminal:"
+    ;;
+  "Trello")
+    icon_result=":trello:"
+    ;;
+  "VMware Fusion")
+    icon_result=":vmware_fusion:"
     ;;
   "VSCodium")
     icon_result=":vscodium:"
@@ -125,6 +197,9 @@ function icon_map() {
     ;;
   "Zen Browser")
     icon_result=":zen_browser:"
+    ;;
+  "zoom.us")
+    icon_result=":zoom:"
     ;;
   *)
     icon_result=":default:"

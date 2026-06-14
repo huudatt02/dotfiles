9dac46eda2270033f8cc41d2c51258c7bc486f16
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-14 16:26:45 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/wifi.sh
+++ sketchybar/.config/sketchybar/items/wifi.sh
@@ -3,11 +3,10 @@
 source "$CONFIG_DIR/icons.sh"
 
 wifi=(
-  label.width=0
   icon="$WIFI_DISCONNECTED"
   script="$PLUGIN_DIR/wifi.sh"
 )
 
 sketchybar --add item wifi right \
            --set wifi "${wifi[@]}" \
-           --subscribe wifi wifi_change mouse.clicked
+           --subscribe wifi wifi_change

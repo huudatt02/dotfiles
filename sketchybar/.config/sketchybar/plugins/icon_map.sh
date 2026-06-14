51a7e2e5119528642f6053ca48f7c8ab7f185c94
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-15 00:15:10 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/plugins/icon_map.sh
+++ sketchybar/.config/sketchybar/plugins/icon_map.sh
@@ -1,23 +1,138 @@
 #!/bin/bash
 
 ### START-OF-ICON-MAP
-function __icon_map() {
-    case "$1" in
-   "Live")
-        icon_result=":ableton:"
-        ;;
-   "Adobe Bridge"*)
-        icon_result=":adobe_bridge:"
-        ;;
-   "Zulip")
-        icon_result=":zulip:"
-        ;;
-    *)
-        icon_result=":default:"
-        ;;
-    esac
+function icon_map() {
+  case "$1" in
+  "Alacritty")
+    icon_result=":alacritty:"
+    ;;
+  "Android Studio")
+    icon_result=":android_studio:"
+    ;;
+  "App Store")
+    icon_result=":app_store:"
+    ;;
+  "Brave Browser")
+    icon_result=":brave_browser:"
+    ;;
+  "Calendar")
+    icon_result=":calendar:"
+    ;;
+  "Chromium" | "Google Chrome" | "Google Chrome Canary")
+    icon_result=":google_chrome:"
+    ;;
+  "Calculator")
+    icon_result=":calculator:"
+    ;;
+  "DataGrip")
+    icon_result=":datagrip:"
+    ;;
+  "FaceTime")
+    icon_result=":face_time:"
+    ;;
+  "Finder")
+    icon_result=":finder:"
+    ;;
+  "Firefox Developer Edition" | "Firefox Nightly")
+    icon_result=":firefox_developer_edition:"
+    ;;
+  "Firefox")
+    icon_result=":firefox:"
+    ;;
+  "GoLand")
+    icon_result=":goland:"
+    ;;
+  "IntelliJ IDEA")
+    icon_result=":idea:"
+    ;;
+  "iTerm" | "iTerm2")
+    icon_result=":iterm:"
+    ;;
+  "kitty")
+    icon_result=":kitty:"
+    ;;
+  "Maps" | "Google Maps")
+    icon_result=":maps:"
+    ;;
+  "Mail")
+    icon_result=":mail:"
+    ;;
+  "Messages")
+    icon_result=":messages:"
+    ;;
+  "MongoDB Compass"*)
+    icon_result=":mongodb:"
+    ;;
+  "Music")
+    icon_result=":music:"
+    ;;
+  "Notes")
+    icon_result=":notes:"
+    ;;
+  "Podcasts")
+    icon_result=":podcasts:"
+    ;;
+  "PhpStorm")
+    icon_result=":php_storm:"
+    ;;
+  "PyCharm")
+    icon_result=":pycharm:"
+    ;;
+  "Reminders")
+    icon_result=":reminders:"
+    ;;
+  "Rider" | "JetBrains Rider")
+    icon_result=":rider:"
+    ;;
+  "Safari")
+    icon_result=":safari:"
+    ;;
+  "Sequel Ace")
+    icon_result=":sequel_ace:"
+    ;;
+  "Sequel Pro")
+    icon_result=":sequel_pro:"
+    ;;
+  "SF Symbols")
+    icon_result=":sf_symbols:"
+    ;;
+  "Spotify")
+    icon_result=":spotify:"
+    ;;
+  "Spotlight")
+    icon_result=":spotlight:"
+    ;;
+  "System Preferences" | "System Settings")
+    icon_result=":gear:"
+    ;;
+  "Terminal")
+    icon_result=":terminal:"
+    ;;
+  "VSCodium")
+    icon_result=":vscodium:"
+    ;;
+  "WezTerm")
+    icon_result=":wezterm:"
+    ;;
+  "WebStorm")
+    icon_result=":web_storm:"
+    ;;
+  "Xcode")
+    icon_result=":xcode:"
+    ;;
+  "Zed")
+    icon_result=":zed:"
+    ;;
+  "Zen Browser")
+    icon_result=":zen_browser:"
+    ;;
+  *)
+    icon_result=":default:"
+    ;;
+  esac
 }
+
 ### END-OF-ICON-MAP
 
-__icon_map "$1"
+icon_map "$1"
 echo "$icon_result"

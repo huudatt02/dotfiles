59ba8730332ca63fcaffa7ef953d39368588f10a
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-16 10:58:28 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/space.sh
+++ sketchybar/.config/sketchybar/items/space.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-##### Adding Mission Control Space Indicators #####
 for monitor in $(aerospace list-monitors --format "%{monitor-appkit-nsscreen-screens-id}"); do
   for sid in $(aerospace list-workspaces --monitor "$monitor" --empty no); do
   done

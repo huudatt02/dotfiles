6ed77581f765bd3de3f217c84d38bc9fa4b6eb8b
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-16 14:25:45 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/front_app.sh
+++ sketchybar/.config/sketchybar/items/front_app.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+chevron=(
+  icon=􀆊
+  icon.font="SF Pro:Bold:16.0"
+  label.drawing=off
+  background.drawing=off
+)
+
 front_app=(
-  label.font="$FONT:Black:12.0"
+  label.font="$FONT:Bold:16.0"
   icon.background.drawing=on
   display=active
   script="$PLUGIN_DIR/front_app.sh"
   click_script="open -a 'Mission Control'"
 )
-sketchybar --add item front_app left         \
+
+sketchybar --add item chevron left \
+           --set chevron "${chevron[@]}"
+
+sketchybar --add item front_app left \
            --set front_app "${front_app[@]}" \
            --subscribe front_app front_app_switched

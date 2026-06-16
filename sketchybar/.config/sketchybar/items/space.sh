1cff055320325cb4b13b3053d60c29bfebc181c7
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-16 12:50:30 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/space.sh
+++ sketchybar/.config/sketchybar/items/space.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sketchybar --add event aerospace_workspace_change
+
 for monitor in $(aerospace list-monitors --format "%{monitor-appkit-nsscreen-screens-id}"); do
   for sid in $(aerospace list-workspaces --monitor "$monitor" --empty no); do
   done

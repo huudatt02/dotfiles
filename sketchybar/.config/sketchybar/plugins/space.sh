226aae2d1d15321ac7a2d24664e6ae6615e6e0ab
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-16 21:53:57 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    sketchybar --set "$NAME" background.drawing=on
 else
-    sketchybar --set $NAME background.drawing=off
+    sketchybar --set "$NAME" background.drawing=off
 fi

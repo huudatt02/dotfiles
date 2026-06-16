90e9191667dc676992bb9c5025a8145e0ac3db12
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-16 13:44:01 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/space.sh
+++ sketchybar/.config/sketchybar/items/space.sh
@@ -4,5 +4,15 @@ sketchybar --add event aerospace_workspace_change
 
 for monitor in $(aerospace list-monitors --format "%{monitor-appkit-nsscreen-screens-id}"); do
   for sid in $(aerospace list-workspaces --monitor "$monitor" --empty no); do
+    sketchybar --add item space.$sid left \
+      --subscribe space.$sid aerospace_workspace_change \
+      --set space.$sid \
+      background.color=0x44ffffff \
+      background.corner_radius=5 \
+      background.height=20 \
+      background.drawing=off \
+      label="$sid" \
+      click_script="aerospace workspace $sid" \
+      script="$CONFIG_DIR/plugins/aerospace.sh $sid"
   done
 done

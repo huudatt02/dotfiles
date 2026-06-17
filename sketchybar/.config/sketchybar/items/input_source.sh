f5265b74f717f0d1153281d243e2a1891fa125f7
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-17 17:06:53 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/input_source.sh
+++ sketchybar/.config/sketchybar/items/input_source.sh
@@ -4,7 +4,9 @@ input_source=(
   label.font="$FONT:Bold:14.0"
   update_freq=1
   script="$PLUGIN_DIR/input_source.sh"
-  )
+)
 
-sketchybar --add item input_source right \
-           --set input_source "${input_source[@]}"
+sketchybar --add event input_source_change 'AppleSelectedInputSourcesChangedNotification' \
+           --add item input_source right \
+           --set input_source "${input_source[@]}" \
+           --subscribe input_source input_source_change

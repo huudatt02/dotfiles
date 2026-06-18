27688ff8ee76d3d82960d93ce8eabb2f00c4f023
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-19 00:15:15 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/battery.lua
+++ sketchybar/.config/sketchybar/items/battery.lua
@@ -8,36 +8,44 @@ local battery = sbar.add("item", {
 			size = 19.0,
 		},
 	},
-	label = { drawing = false },
+	label = {
+		font = {
+			style = "Semibold",
+			size = 14.0,
+		},
+	},
 	update_freq = 120,
 })
 
 local function battery_update()
 	sbar.exec("pmset -g batt", function(batt_info)
 		local icon = "!"
+		local percentage = ""
+
+		local found, _, charge = batt_info:find("(%d+)%%")
+		if found then
+			percentage = charge .. "%"
+			charge = tonumber(charge)
+		end
 
 		if string.find(batt_info, "AC Power") then
 			icon = icons.battery.charging
+		elseif charge > 80 then
+			icon = icons.battery._100
+		elseif charge > 60 then
+			icon = icons.battery._75
+		elseif charge > 40 then
+			icon = icons.battery._50
+		elseif charge > 20 then
+			icon = icons.battery._25
 		else
-			local found, _, charge = batt_info:find("(%d+)%%")
-			if found then
-				charge = tonumber(charge)
-			end
-
-			if found and charge > 80 then
-				icon = icons.battery._100
-			elseif found and charge > 60 then
-				icon = icons.battery._75
-			elseif found and charge > 40 then
-				icon = icons.battery._50
-			elseif found and charge > 20 then
-				icon = icons.battery._25
-			else
-				icon = icons.battery._0
-			end
+			icon = icons.battery._0
 		end
 
-		battery:set({ icon = icon })
+		battery:set({
+			icon = icon,
+			label = percentage,
+		})
 	end)
 end
 

57f546fd4024585576761cbebf676e99c62f9c44
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-18 22:52:15 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/plugins/calendar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source "$CONFIG_DIR/icons.sh"
-
-sketchybar --set "$NAME" icon="$CALENDAR" label="$(date +'%a %d %b %H:%M')"
-
--- sketchybar/.config/sketchybar/plugins/wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source "$CONFIG_DIR/icons.sh"
-
-IP="$(ipconfig getifaddr en0 2>/dev/null)"
-ICON="$([ -n "$IP" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
-
-sketchybar --set "$NAME" icon="$ICON"

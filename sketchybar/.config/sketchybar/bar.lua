1e57e9a2f1178864d6c3709c5ddf1d8d8478aca7
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-18 23:54:30 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/bar.lua
+++ sketchybar/.config/sketchybar/bar.lua
@@ -3,12 +3,12 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
 	height = 40,
-	color = colors.bar.bg,
+	color = colors.system.transparent,
 	border_color = colors.bar.border,
 	shadow = true,
 	sticky = true,
 	padding_right = 10,
 	padding_left = 10,
-	blur_radius = 20,
+	blur_radius = 0,
 	topmost = "window",
 })

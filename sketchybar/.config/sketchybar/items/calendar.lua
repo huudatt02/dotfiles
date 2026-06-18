739dcb610896542a49020dfba40bb5f4564d5566
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-19 00:22:59 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/calendar.lua
+++ sketchybar/.config/sketchybar/items/calendar.lua
@@ -1,21 +1,24 @@
 local cal = sbar.add("item", {
 	icon = {
-		padding_right = 0,
 		font = {
-			style = "Black",
-			size = 12.0,
+			style = "Semibold",
+			size = 14.0,
 		},
 	},
 	label = {
 		width = 45,
 		align = "right",
+		font = {
+			style = "Semibold",
+			size = 14.0,
+		},
 	},
 	position = "right",
 	update_freq = 15,
 })
 
 local function update()
-	local date = os.date("%a. %d %b.")
+	local date = os.date("%a %d %b")
 	local time = os.date("%H:%M")
 	cal:set({ icon = date, label = time })
 end

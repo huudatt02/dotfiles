0f37dfed34d374788c72ad8a3015841d9cadf9eb
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-19 10:31:08 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/settings.lua
+++ sketchybar/.config/sketchybar/settings.lua
@@ -1,4 +1,19 @@
 return {
-	font = "SF Pro",
 	paddings = 3,
+	group_paddings = 5,
+
+	icons = "sf-symbols",
+
+	font = {
+		text = "SF Pro",
+		numbers = "SF Mono",
+
+		style_map = {
+			["Regular"] = "Regular",
+			["Semibold"] = "Semibold",
+			["Bold"] = "Bold",
+			["Heavy"] = "Heavy",
+			["Black"] = "Black",
+		},
+	},
 }

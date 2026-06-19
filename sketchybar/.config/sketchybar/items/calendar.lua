48888ab4016d61183266e851a445c521794d8d62
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-19 11:07:10 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/calendar.lua
+++ sketchybar/.config/sketchybar/items/calendar.lua
@@ -1,29 +1,27 @@
-local settings = require("settings")
-local colors = require("colors")
-
 local cal = sbar.add("item", {
 	icon = {
-		color = colors.system.white,
 		font = {
-			style = settings.font.style_map["Semibold"],
+			style = "Semibold",
 			size = 14.0,
 		},
 	},
 	label = {
-		color = colors.system.white,
 		width = 45,
 		align = "right",
 		font = {
-			family = settings.font.numbers,
-			style = settings.font.style_map["Semibold"],
+			style = "Semibold",
 			size = 14.0,
 		},
 	},
 	position = "right",
-	update_freq = 30,
-  click_script = "open -a 'Calendar'"
+	update_freq = 15,
 })
 
-cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
-  cal:set({ icon = os.date("%a %d %b"), label = os.date("%H:%M") })
-end)
+local function update()
+	local date = os.date("%a %d %b")
+	local time = os.date("%H:%M")
+	cal:set({ icon = date, label = time })
+end
+
+cal:subscribe("routine", update)
+cal:subscribe("forced", update)

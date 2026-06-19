fa74018252249bdbee33fb97f81f4a6f88376d6e
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-19 15:57:00 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/volume.lua
+++ sketchybar/.config/sketchybar/items/volume.lua
@@ -1,112 +1,127 @@
 local icons = require("icons")
 
 local volume = sbar.add("item", "volume", {
-  position = "right",
+	position = "right",
 
-  icon = {
-    string = icons.volume.medium,
-  },
+	icon = {
+		string = icons.volume.medium,
+	},
 
-  label = {
-    string = "--%",
-    width = 0,
-  },
+	label = {
+		string = "--%",
+		width = 0,
+	},
 
-  updates = true,
+	updates = true,
 })
 
 local hide_id = 0
+local label_visible = false
 
 local function show_label()
-  hide_id = hide_id + 1
-  local current_id = hide_id
+	hide_id = hide_id + 1
+	local current_id = hide_id
 
-  local width = tonumber(volume:query().label.width) or 0
+	if not label_visible then
+		label_visible = true
 
-  if width == 0 then
-    sbar.animate("tanh", 20, function()
-      volume:set({
-        label = {
-          width = 42,
-        },
-      })
-    end)
-  else
-    volume:set({
-      label = {
-        width = 42,
-      },
-    })
-  end
+		sbar.animate("tanh", 20, function()
+			volume:set({
+				label = {
+					width = 45,
+				},
+			})
+		end)
+	end
 
-  sbar.exec("sleep 2", function()
-    if current_id ~= hide_id then
-      return
-    end
+	sbar.exec("sleep 2", function()
+		if current_id ~= hide_id then
+			return
+		end
 
-    sbar.animate("tanh", 20, function()
-      volume:set({
-        label = {
-          width = 0,
-        },
-      })
-    end)
-  end)
+		label_visible = false
+
+		sbar.animate("tanh", 20, function()
+			volume:set({
+				label = {
+					width = 0,
+				},
+			})
+		end)
+	end)
 end
 
 local function icon_for_volume(v)
-  if v == 0 then
-    return icons.volume.mute
-  elseif v <= 20 then
-    return icons.volume.min
-  elseif v <= 40 then
-    return icons.volume.low
-  elseif v <= 70 then
-    return icons.volume.medium
-  else
-    return icons.volume.high
-  end
+	if v == 0 then
+		return icons.volume.mute
+	elseif v < 33 then
+		return icons.volume.low
+	elseif v < 66 then
+		return icons.volume.medium
+	else
+		return icons.volume.high
+	end
 end
 
 local function update_volume(env)
-  local v = tonumber(env.INFO)
+	local v = tonumber(env.INFO)
 
-  if not v then
-    return
-  end
+	if not v then
+		return
+	end
 
-  volume:set({
-    icon = {
-      string = icon_for_volume(v),
-    },
+	volume:set({
+		icon = {
+			string = icon_for_volume(v),
+		},
 
-    label = {
-      string = ("%d%%"):format(v),
-    },
-  })
+		label = {
+			string = ("%d%%"):format(v),
+		},
+	})
 
-  show_label()
+	show_label()
 end
 
 volume:subscribe("volume_change", update_volume)
 
 volume:subscribe("mouse.clicked", function()
-  sbar.exec(
-    "osascript -e 'output volume of (get volume settings)'",
-    function(output)
-      local v = tonumber(output)
+	sbar.exec("osascript -e 'output volume of (get volume settings)'", function(output)
+		local v = tonumber(output)
 
-      if not v then
-        return
-      end
+		if not v then
+			return
+		end
 
-      volume:set({
-        label = {
-          string = ("%d%%"):format(v),
-        },
-      })
+		volume:set({
+			icon = {
+				string = icon_for_volume(v),
+			},
 
-      show_label()
-    end
-  )
+			label = {
+				string = ("%d%%"):format(v),
+			},
+		})
+
+		show_label()
+	end)
+end)
+
+-- Initial sync on startup/reload
+sbar.exec("osascript -e 'output volume of (get volume settings)'", function(output)
+	local v = tonumber(output)
+
+	if not v then
+		return
+	end
+
+	volume:set({
+		icon = {
+			string = icon_for_volume(v),
+		},
+
+		label = {
+			string = ("%d%%"):format(v),
+		},
+	})
 end)

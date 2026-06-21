99fea43f2fbe1ae0a840ed9fd208adad2c1c9273
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-21 17:19:24 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/front_app.lua
+++ sketchybar/.config/sketchybar/items/front_app.lua
@@ -1,26 +1,50 @@
-local front_app = sbar.add("item", {
-	icon = {
+sbar.add("item", "chevron", {
+	position = "left",
+	background = {
 		drawing = false,
+	},
+	icon = {
+		string = "􀆊",
+		font = {
+			style = "Bold",
+			size = 16,
+		},
+	},
+	label = {
+		drawing = false,
+	},
+})
+
+local front_app = sbar.add("item", "front_app", {
+	position = "left",
+	display = "active",
+	icon = {
+		background = {
+			drawing = true,
+		},
 	},
 	label = {
 		font = {
 			style = "Bold",
-			size = 14.0,
+			size = 14,
 		},
 	},
+	click_script = "open -a 'Mission Control'",
 })
 
 front_app:subscribe("front_app_switched", function(env)
+	local app = env.INFO
+
 	front_app:set({
-		label = {
-			string = env.INFO,
+		label = app,
+
+		icon = {
+			background = {
+				image = {
+					string = "app." .. app,
+					scale = 0.8,
+				},
+			},
 		},
 	})
-
-	-- Or equivalently:
-	-- sbar.set(env.NAME, {
-	--   label = {
-	--     string = env.INFO
-	--   }
-	-- })
 end)

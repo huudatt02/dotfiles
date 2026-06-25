a5c9ee4ffe93d7b8fd9d65a76a9f85eb95b470e7
huudatt02 <huudatnguyen1912@gmail.com>
2026-06-25 13:55:10 +0700

    chore: add tool configurations

--- sketchybar/.config/sketchybar/items/spaces.lua
+++ sketchybar/.config/sketchybar/items/spaces.lua
@@ -184,6 +184,58 @@ sbar.exec(query_workspaces, function(workspaces_and_monitors)
 		end)
 	end
 
+	sbar.add("item", "chevron", {
+		position = "left",
+		display = "active",
+		background = {
+			drawing = false,
+		},
+		icon = {
+			string = "􀆊",
+			font = {
+				style = "Bold",
+				size = 16.0,
+			},
+		},
+		label = {
+			drawing = false,
+		},
+	})
+
+	local front_app = sbar.add("item", "front_app", {
+		position = "left",
+		display = "active",
+		icon = {
+			background = {
+				drawing = true,
+			},
+		},
+		label = {
+			font = {
+				style = "Bold",
+				size = 14.0,
+			},
+		},
+		click_script = "open -a 'Mission Control'",
+	})
+
+	front_app:subscribe("front_app_switched", function(env)
+		local app = env.INFO
+
+		front_app:set({
+			label = app,
+
+			icon = {
+				background = {
+					image = {
+						string = "app." .. app,
+						scale = 0.8,
+					},
+				},
+			},
+		})
+	end)
+
 	-- Initial setup
 	updateWindows()
 	updateWorkspaceMonitor()

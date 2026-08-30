30d265477fa95eb780fa08677554b5f7f91468ee
huudatt02 <huudatnguyen1912@gmail.com>
2026-08-30 13:45:32 +0700

    chore(nvim): add plugin venv-selector.nvim

---
+++ nvim/.config/nvim/lua/huudatt02/plugins/venv-selector.lua
@@ -0,0 +1,14 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  cmd = "VenvSelect",
+  opts = {
+    options = {
+      notify_user_on_venv_activation = true,
+      override_notify = false,
+    },
+  },
+  ft = "python",
+  keys = {
+    { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" },
+  },
+}

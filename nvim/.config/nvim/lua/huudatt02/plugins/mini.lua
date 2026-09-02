066547a5e4d23dc07cb80b3dbcc362e80d43bb4a
huudatt02 <huudatnguyen1912@gmail.com>
2026-09-02 23:42:44 +0700

    chore(nvim): add lint ruff for python

--- nvim/.config/nvim/lua/huudatt02/plugins/mini.lua
+++ nvim/.config/nvim/lua/huudatt02/plugins/mini.lua
@@ -7,10 +7,12 @@ return {
       file = {
         [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
         ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
       },
       filetype = {
         env = { glyph = "", hl = "MiniIconsYellow" },
         dotenv = { glyph = "", hl = "MiniIconsYellow" },
+        gotmpl = { glyph = "󰟓", hl = "MiniIconsGrey" },
       },
     },
     init = function()

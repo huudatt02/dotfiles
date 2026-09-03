28385f76d052418420f5f4d763ea0954d1be21ab
huudatt02 <huudatnguyen1912@gmail.com>
2026-09-03 23:16:37 +0700

    chore(nvim): add lint ruff for python

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,6 +9,7 @@ config.macos_window_background_blur = 20
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.default_cursor_style = 'SteadyBar'
+config.window_close_confirmation = 'NeverPrompt'
 
 config.window_padding = {
     left = 10,

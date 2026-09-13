ec5b9908780268b0587e03420de111dfebdcb21b
huudatt02 <huudatnguyen1912@gmail.com>
2026-09-13 13:34:59 +0700

    config ghostty

--- nvim/.config/nvim/lua/huudatt02/plugins/lint.lua
+++ nvim/.config/nvim/lua/huudatt02/plugins/lint.lua
@@ -16,6 +16,7 @@ return {
       typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
+      json = { "jsonlint" },
     }
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
